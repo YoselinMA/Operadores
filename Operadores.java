79afdc17a03276271e8fedb1f94cf1c7eca6e5b9
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-03 21:59:01 -0600

    Create Operadores.java

---
+++ Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int p = 5;
+    int q = 1;
+    int r = 2;
+    int w = 3;
+    int x = 9;
+    int y = 6;
+    int z;
+
+    //Impresion de números negativos
+    z = p * r % q + w / x - y;
+    System.out.println("z: "+z);
+    }
+    
+}
